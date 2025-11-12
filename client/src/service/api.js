7a4e5fd01c0b0a1bbad2d364a6d97c2b67cf7105
khushii <khushii@Khushis-MacBook-Pro.local>
2025-11-12 17:24:00 +0530

    api

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE = import.meta.env.VITE_API_BASE || 'https://qverse-4.onrender.com/api';
+export const API_BASE = import.meta.env.VITE_API_BASE || 'https://qverse-5.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE,
