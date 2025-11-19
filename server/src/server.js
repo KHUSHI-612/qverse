bbfa43bd2b9828b64fae850e5a85e3cf84cd2bc6
khushii <khushii@Khushis-MacBook-Pro.local>
2025-11-19 12:29:49 +0530

    backend fix

--- server/src/server.js
+++ server/src/server.js
@@ -11,6 +11,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({
   origin: [
     'http://localhost:5173',
